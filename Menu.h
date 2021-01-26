999b184f1e7c82453e199faf85f3b61f37c3c004
sina shariati <sina.shariati@yahoo.com>
2021-01-26 12:19:22 +0330

    Move Menu.h to be in base folder

--- Menu.h
+++ Menu.h
@@ -1,8 +1,8 @@
-//
-// Created by sinas on 1/26/2021.
-//
-
 #ifndef SEA_BATTLE_MENU_H
 #define SEA_BATTLE_MENU_H
+
+int getUserMenuOrder(void (*menuShowCallback)());
+
+void showMainMenu();
 
 #endif //SEA_BATTLE_MENU_H
