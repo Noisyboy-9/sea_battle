709246c96df1e841f79527be39328e3953065ae0
sina shariati <sina.shariati@yahoo.com>
2021-01-26 11:36:48 +0330

    init commit

---
+++ cmake-build-debug/CMakeFiles/sea_battle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sea_battle.dir/main.c.o"
+  "sea_battle"
+  "sea_battle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sea_battle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
