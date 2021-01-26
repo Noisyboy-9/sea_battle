9f15583467d5567cf3dc921c56676d1344cf48b5
sina shariati <sina.shariati@yahoo.com>
2021-01-26 12:57:50 +0330

    Move menu.h to menu folder

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "menu.h"
+#include "menu/menu.h"
 
 int main() {
     int mainMenuOrder = getUserMenuOrder(&showMainMenu);
--- menu/menu.h
+++ menu/menu.h
@@ -1,7 +1,7 @@
 #ifndef SEA_BATTLE_MENU_H
 #define SEA_BATTLE_MENU_H
 
-#include "menu/main/main_menu.h"
-#include "menu/helpers/menu_helpers.h"
+#include "main/main_menu.h"
+#include "helpers/menu_helpers.h"
 
 #endif //SEA_BATTLE_MENU_H
