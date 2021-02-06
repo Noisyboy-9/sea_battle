49bd946e54a7e94d8897f53379a67fb71274d404
sina shariati <sina.shariati@yahoo.com>
2021-02-06 20:30:25 +0330

    Finish define ships

---
+++ ship/define/ship_define.h
@@ -0,0 +1,9 @@
+#ifndef SEA_BATTLE_SHIP_DEFINE_H
+#define SEA_BATTLE_SHIP_DEFINE_H
+typedef struct {
+    int width;
+    int height;
+    Coordinate start;
+    Coordinate finish;
+} Ship;
+#endif //SEA_BATTLE_SHIP_DEFINE_H
--- ship/ship.h
+++ ship/ship.h
@@ -1,4 +1,6 @@
 #ifndef SEA_BATTLE_SHIP_H
 #define SEA_BATTLE_SHIP_H
 
+#include "define/ship_define.h"
+
 #endif //SEA_BATTLE_SHIP_H
